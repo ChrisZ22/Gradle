1989d9274cc9a8263a905a69b14d97e620d6fd7c
GitHub Action <action@github.com>
2022-03-20 17:28:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/chrisz22/gradle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chrisz22/gradle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chrisz22.gradle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chrisz22.gradle.MyBundle
 
 class MyApplicationService {
 
